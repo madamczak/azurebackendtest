2c120dda250ea7f8996c64e273ce1b208f849ded
adamczak.mateusz@gmail.com <adamczak.mateusz@gmail.com>
2021-12-08 20:15:37 +0100

    second commit

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import uvicorn as uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -6,3 +7,6 @@ app = FastAPI()
 @app.get("/getmsg")
 async def root():
     return {"message": "Hello World"}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False)
